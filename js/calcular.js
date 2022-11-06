528f074e7e3932d93397fafe4f8d7d1da6b7e150
Chesini Pablo <pchesini1@gmail.com>
2022-11-06 18:41:15 -0300

    funcionalidad con js completa

--- js/calcular.js
+++ js/calcular.js
@@ -1,65 +1,33 @@
-/* let nombre;
-let apellido;
 
-traigo la cantidad y calculo el valor del tiquets
-function calcular(){
-    let cantidad;
-    let valorTiket = 200;
-
-    cantidad = document.getElementById("inputCant").value;
-
-    valor = cantidad * valorTiket;
-    porc(valor)
-
-}
-traigo el descuento y calculo su poprcentaje
-function porc(val,){
-    let categoria;
-    categoria = getElementById("inputCat").value
-    total = (val * categoria) / 100;
-    return total;
-}
-*/ 
-const cantTiket = document.getElementById('inputCant')
-
-const btnCalcular = document.getElementById('btnResumen')
-
-const categoria = document.getElementById('inputSelect')
-
-btnResumen.addEventListener('click',resumen)
-
-const btnBorrar = document.getElementById('btnBorrar')
-
-botonBorrar.addEventListener('click', borrar)
+const cantTiket = document.getElementById('inputCant');
+const btnCalcular = document.getElementById('btnResumen');
+const categoria = document.getElementById('inputSelect');
+const btnBorrar = document.getElementById('btnBorrar');
+btnCalcular.onclick = resumen;
+btnBorrar.onclick = borrar;
 
 
 function resumen(){
 
-  totalPagar =  calcularImporte(categoria.value, cantidadEntradas.value);
+  totalSinDescuento =  calcularImporte(cantTiket.value);
+  totalPagar = totalSinDescuento - descuentoPorcentaje (categoria.value,totalSinDescuento)
 
-  if(cantidadEntradas.value == 0 || cantidadEntradas.value == ''){
-    alert("DEBE INGRESAR LA CANTIDAD DE ENTRADAS QUE DESEA ADQUIRIR")
+  if(cantTiket.value == 0 || cantTiket.value == ''){
+    alert("Ingrese una cantidad valida")
   }
+  
 
-  totalCompra.innerHTML=  `Total a Pagar: $ ${totalPagar}`
+  totalCompra.innerHTML = `Total a Pagar: $ ${totalPagar}`
 
 }
 
-const calcularImporte = (categoria, cantidad) => {
-  valorTicket = 200;
-  totalPagar = 0;
-
-  descuentos = {
-    estudiante: 0.8,
-    trainee: 0.5,
-    junior: 0.15
-  }
-
-  descuentoTotal = valorTicket * descuentos[`${categoria}`] * cantidad;
-
-  return totalPagar = valorTicket * cantidad - descuentoTotal
-
+function calcularImporte (cantidad){
+  const valorTicket = 200;
+  return cantidad * valorTicket;
 }
+ function descuentoPorcentaje(desc,valorImp){
+     return desc * valorImp;
+ }
 
 function borrar () {
   totalCompra.innerHTML=  `Total a Pagar: $ 0`
